225d8c5ee5694d61c1e22fcb6504a881bdd231d7
Geoffrey <geoffreey.bernard@isen.yncrea.fr>
2021-05-01 11:35:51 +0200

    Dépacements gauche-droite et flip et tilemap

--- Assets/Inputs/PlayerInputs.cs
+++ Assets/Inputs/PlayerInputs.cs
@@ -33,6 +33,14 @@ public class @PlayerInputs : IInputActionCollection, IDisposable
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""e1d4b5ae-0405-41f2-85e1-8f5b280de8a2"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
                 }
             ],
             ""bindings"": [
@@ -220,6 +228,17 @@ public class @PlayerInputs : IInputActionCollection, IDisposable
                     ""processors"": """",
                     ""groups"": ""XR"",
                     ""action"": ""Fire"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""35b2738b-0540-47af-bc85-6b6491ca1741"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard&Mouse"",
+                    ""action"": ""Jump"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -799,6 +818,7 @@ public class @PlayerInputs : IInputActionCollection, IDisposable
         m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
         m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
         m_Player_Fire = m_Player.FindAction("Fire", throwIfNotFound: true);
+        m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
         // UI
         m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
         m_UI_Navigate = m_UI.FindAction("Navigate", throwIfNotFound: true);
@@ -862,12 +882,14 @@ public class @PlayerInputs : IInputActionCollection, IDisposable
     private IPlayerActions m_PlayerActionsCallbackInterface;
     private readonly InputAction m_Player_Move;
     private readonly InputAction m_Player_Fire;
+    private readonly InputAction m_Player_Jump;
     public struct PlayerActions
     {
         private @PlayerInputs m_Wrapper;
         public PlayerActions(@PlayerInputs wrapper) { m_Wrapper = wrapper; }
         public InputAction @Move => m_Wrapper.m_Player_Move;
         public InputAction @Fire => m_Wrapper.m_Player_Fire;
+        public InputAction @Jump => m_Wrapper.m_Player_Jump;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -883,6 +905,9 @@ public class @PlayerInputs : IInputActionCollection, IDisposable
                 @Fire.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnFire;
                 @Fire.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnFire;
                 @Fire.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnFire;
+                @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
             }
             m_Wrapper.m_PlayerActionsCallbackInterface = instance;
             if (instance != null)
@@ -893,6 +918,9 @@ public class @PlayerInputs : IInputActionCollection, IDisposable
                 @Fire.started += instance.OnFire;
                 @Fire.performed += instance.OnFire;
                 @Fire.canceled += instance.OnFire;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
             }
         }
     }
@@ -1051,6 +1079,7 @@ public class @PlayerInputs : IInputActionCollection, IDisposable
     {
         void OnMove(InputAction.CallbackContext context);
         void OnFire(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
     }
     public interface IUIActions
     {
--- Assets/Script/Snatchy.cs
+++ Assets/Script/Snatchy.cs
@@ -1,45 +1,47 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Snatchy : MonoBehaviour
 {
-    private float gravityMultiplier = 1f;
-
-    private Rigidbody2D rgdb;
     private PlayerInputs inputs;
 
-    private Vector2 moveAxis;
+    [SerializeField] private float speed = 150f;
 
-    Vector2 velocity;
+    private Vector2 moveAxis;
+    private Rigidbody2D rgdb;
+
+    bool isFacingRight = true;
 
     private void Awake()
     {
         rgdb = GetComponent<Rigidbody2D>();
-        
+
         inputs = new PlayerInputs();
 
         inputs.Player.Move.performed += ctx => OnMove(ctx.ReadValue<Vector2>());
         inputs.Player.Move.canceled += ctx => OnMove(Vector2.zero);
+
+        inputs.Player.Jump.performed += ctx => OnJump();
     }
 
     private void Start()
     {
-        
+
     }
 
     private void Update()
     {
-        rgdb.velocity = velocity;
+        if ((rgdb.velocity.x > 0 && !isFacingRight) || (rgdb.velocity.x < 0 && isFacingRight)) Flip();
     }
 
     private void FixedUpdate()
     {
-        velocity += gravityMultiplier * Physics2D.gravity * Time.deltaTime;
+        rgdb.velocity = moveAxis * speed * Time.deltaTime;
     }
 
     private void OnMove(Vector2 input)
     {
         moveAxis = input;
+        moveAxis.y = 0f;
     }
 
     private void OnEnable()
@@ -50,5 +52,19 @@ public class Snatchy : MonoBehaviour
     private void OnDisable()
     {
         inputs.Disable();
+    }
+
+    private void OnJump()
+    {
+
+    }
+
+    private void Flip()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+
+        isFacingRight = !isFacingRight;
     }
 }
